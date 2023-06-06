bee90b12f52a0e55eef18a9d39c2f19d0b862d2e
DaniilMiliukov <120185973+DaniilMiliukov@users.noreply.github.com>
2023-06-06 11:28:51 +0000

    Deploying to gh-pages from @ DaniilMiliukov/goit-js-hw-11@6e4805d7fda4e096eca73ec9a31529e7d415c08f 🚀

---
+++ index.0ee47c09.js
@@ -0,0 +1,2 @@
+function o(){let o="#";for(let t=0;t<6;t++)o+="0123456789ABCDEF"[Math.floor(16*Math.random())];return o}function t(o){return new Promise((t=>setTimeout(t,o)))}!async function(){for(;;){const n=o();document.querySelector(".section-form").style.backgroundColor=n,await t(2500)}}();
+//# sourceMappingURL=index.0ee47c09.js.map
